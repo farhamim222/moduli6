853a119d9b02d38a6ddb7ecae5f62b9c89071fc5
farha <Farha.Mim@metropolia.fi>
2024-09-04 01:52:49 +0300

    done

---
+++ 04.py
@@ -0,0 +1,13 @@
+def sum_of_list(number_list):
+    return sum(number_list)
+
+def main():
+
+    test_list = [5, 12, 7, -3, 8]
+
+    total_sum = sum_of_list(test_list)
+
+    # Print the sum
+    print(f"The sum of the numbers in the list is: {total_sum}")
+
+main()
